5071a76ff3570591e3206536f83bff8b09b234ce
itslenny <lenny@itslennysfault.com>
2016-08-27 12:40:22 -0700

    Add walking test

---
+++ src/Assets/Scripts/TestPlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class TestPlayerController : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody2D rb2d;
+	private Animator animator;
+	private bool facingRight;
+	private string animationState;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		facingRight = false;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		if (Input.GetKeyDown ("space")) {
+			setAnimation("Attack");
+		}
+		else if (moveHorizontal == 0 && moveVertical == 0) {
+			setAnimation("Idle");
+		} else {
+			setAnimation("Walk");
+			if (moveHorizontal > 0 != facingRight) {
+				flip ();
+			}
+		}
+
+
+
+//		animator.SetFloat ("speed", Mathf.Abs(moveHorizontal));
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical) * speed;
+		rb2d.AddForce (movement);
+	}
+
+	private void flip() 
+	{
+		facingRight = !facingRight;
+		Vector3 currentScale = transform.localScale;
+		currentScale.x *= -1;
+		transform.localScale = currentScale;
+	}
+
+	private void setAnimation(string state) {
+		if(animationState != state) {
+			if (!String.IsNullOrEmpty(animationState)) {
+				animator.ResetTrigger (animationState);
+			}
+			animator.SetTrigger (state);
+			animationState = state;
+		}
+	}
+}
---
+++ src/Assets/Scripts/ZIndexSorter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZIndexSorter : MonoBehaviour {
+
+	private BoxCollider2D collide2d;
+
+	// Use this for initialization
+	void Start () {
+		calculateZ ();	
+		collide2d = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		calculateZ ();
+	}
+
+	private void calculateZ()
+	{
+		if (collide2d == null) {
+			return;
+		}
+		Vector3 pos = transform.position;
+		pos.z = collide2d.bounds.max.y;
+		transform.position = pos;		
+
+	}
+}
