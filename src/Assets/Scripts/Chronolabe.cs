25218c28f9fa2c41f4b3949efbfd19d095e153f3
Jonathan Borzilleri <jonathan@borzilleri.net>
2016-08-27 01:51:31 -0700

    Initial Chronolabe/ghost work

---
+++ src/Assets/Scripts/Chronolabe.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chronolabe : MonoBehaviour, UsableObject
+{
+	public int Duration;
+
+	int recordingCount = 0;
+
+	bool isRecording = false;
+	bool doSpawn = false;
+
+	List<GhostController> ghosts;
+
+	PlayerController user;
+	Vector3 currentSpawn;
+	List<PlayerFrameAction> currentActions;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		ghosts = new List<GhostController> ();
+		Duration = 60 * 10;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (doSpawn) {
+			currentSpawn = user.transform.position;
+			currentActions = new List<PlayerFrameAction> ();
+
+			// Activate Existing Ghosts.
+			foreach (var ghost in ghosts) {
+				ghost.gameObject.SetActive (true);
+			}
+			doSpawn = false;
+		}
+	}
+
+	void LateUpdate ()
+	{
+		if (isRecording) {
+			if (recordingCount > Duration) {
+				isRecording = false;
+				recordingCount = 0;
+				ghosts.Add (GhostController.Create (currentActions, currentSpawn));
+				user = null;
+			} else {
+				currentActions.Add (user.lastAction);
+				recordingCount += 1;
+			}
+		}
+	}
+
+	public void Use (GameObject user)
+	{
+		if (!isRecording) {
+			isRecording = true;
+			doSpawn = true;
+
+			this.user = user.GetComponent<PlayerController>();
+		}
+	}
+}
--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -6,23 +6,43 @@ public class PlayerController : MonoBehaviour
 	private Rigidbody2D rb2d;
 	public float speed;
 
+	public PlayerFrameAction lastAction;
+	bool isUsing;
+
 	// Use this for initialization
 	void Start ()
 	{
+		isUsing = false;
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if (Input.GetButtonDown ("space")) {
+			// Use whatever we're near.
+		}
 	}
 
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce (movement * speed);
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical) * speed;
+		rb2d.AddForce (movement);
+
+		lastAction = new PlayerFrameAction (movement, isUsing);
 	}
+
+	void LateUpdate() {
+
+		// This is where we check for is recording, and push lastAction onto the current recording stack.
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+	}
+		
 }
