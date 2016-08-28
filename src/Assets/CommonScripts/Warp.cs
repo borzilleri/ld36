3c0a547c86c21799aa4990c76bb03ff87d2c28a9
Tim Sendgikoski <Tim Sendgikoski>
2016-08-27 17:24:08 -0700

    Implements a basic object activator for standard actions

--- src/Assets/CommonScripts/Warp.cs
+++ src/Assets/CommonScripts/Warp.cs
@@ -1,13 +1,1 @@
-﻿using UnityEngine;
-
-public class Warp : MonoBehaviour {
-
-    public Transform warpTarget;
-
-	void OnTriggerEnter2D(Collider2D collidee)
-    {
-        Debug.Log(string.Format("Object {0} collided", collidee.name));
-        collidee.gameObject.transform.position = warpTarget.position;
-        // Camera.main.transform.position = warpTarget.position;  // Use when camera follows player
-    }
-}
+﻿using UnityEngine;public class Warp : MonoBehaviour {    public Transform warpTarget;	void OnTriggerEnter2D(Collider2D collidee)    {        Debug.Log(string.Format("Object {0} collided", collidee.name));        collidee.gameObject.transform.position = warpTarget.position;        // Camera.main.transform.position = warpTarget.position;  // Use when camera follows player    }}
