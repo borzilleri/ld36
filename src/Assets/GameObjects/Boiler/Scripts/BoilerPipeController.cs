8acaea996760e46c0048554c3dc26cd0b0b0ef68
itslenny <lenny@itslennysfault.com>
2016-08-29 16:17:29 -0700

    fixed ghost pipe issue

--- src/Assets/GameObjects/Boiler/Scripts/BoilerPipeController.cs
+++ src/Assets/GameObjects/Boiler/Scripts/BoilerPipeController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class BoilerPipeController : MonoBehaviour {
 
 	public BoilerMainController boiler;
+	private bool triggered = false;
+	private Collider2D other;
 
 	private Animator animator;
 
@@ -12,16 +14,28 @@ public class BoilerPipeController : MonoBehaviour {
 		animator = GetComponent<Animator> ();
 	}
 
+	void Update()
+	{
+		if (triggered && (null == other || !other.isActiveAndEnabled)) {
+			animator.SetBool ("Fixed", false);
+			boiler.pipe = false;
+			triggered = false;
+		}
+	}
+
 	public void OnTriggerEnter2D (Collider2D user)
 	{
 		animator.SetBool ("Fixed", true);
 		boiler.pipe = true;
+		triggered = true;
+		other = user;
 	}
 
 	public void OnTriggerExit2D (Collider2D user)
 	{
 		animator.SetBool ("Fixed", false);
 		boiler.pipe = false;
+		triggered = false;
 	}
 
 }
