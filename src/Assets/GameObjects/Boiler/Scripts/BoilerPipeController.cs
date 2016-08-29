b2856e64898d3dfef0597e84afb3bff1597a6634
Jonathan Borzilleri <jonathan@borzilleri.net>
2016-08-29 01:23:27 -0700

    Fixes, waterfall enabled, stuff.

---
+++ src/Assets/GameObjects/Boiler/Scripts/BoilerPipeController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoilerPipeController : MonoBehaviour, UsableObject {
+
+	public void UseStart(GameObject user) {
+		GetComponent<Animator> ().SetBool ("Fixed", true);
+	}
+	public void UseEnd(GameObject user) {
+		GetComponent<Animator> ().SetBool ("Fixed", false);
+	}
+	public void Nearby(GameObject user) {
+	}
+
+}
