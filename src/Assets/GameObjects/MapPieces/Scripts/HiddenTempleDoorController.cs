2767a8c291db50a7c6c47ab86ade8fa0c2977cc3
itslenny <lenny@itslennysfault.com>
2016-08-29 01:11:41 -0700

    Rearrange, add chronolabe new

---
+++ src/Assets/GameObjects/MapPieces/Scripts/HiddenTempleDoorController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenTempleDoorController : MonoBehaviour {
+
+	private bool isOpen = false;
+	private Animator animator;
+	private BoxCollider2D bc2d;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		bc2d = GetComponent<BoxCollider2D> ();
+//		setDoorOpen (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void setDoorOpen(bool open) {
+		animator.Play (open ? "HiddenTempleDoorOpen" : "HiddenTempleDoorClose");
+		bc2d.enabled = open;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("and aawwaaaay we go!!!");
+	}
+}
+
